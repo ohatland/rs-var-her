63a2fb18dee221af5eeae56049494525889653d1
Øyvind Hatland <oyvind.hatland@yahoo.com>
2024-07-10 16:01:54 +0200

    show timestamp and ship icon in track

--- src/components/map/map-widget.ts
+++ src/components/map/map-widget.ts
@@ -1,23 +1,30 @@
 import "leaflet/dist/leaflet.css";
 import * as L from "leaflet";
+//@ts-ignore
+import { nauticalScale } from './leaflet/NauticalScaleControl';
+import './leaflet/RotatedMarker';
 
 export class MapWidget {
   private map: L.Map;
   private layer: L.TileLayer | null = null;
   private tracks: L.Polyline[] = [];
+  private markers: L.Marker[] = [];
+  private icons: L.Icon[] = [];
+
+  private shipIcon = L.icon({
+    iconUrl: 'src/assets/ship.png',
+
+    iconSize: [16, 16],
+    iconAnchor: [8, 10]
+  })
 
   constructor(domNode: HTMLElement) {
     this.map = L.map(domNode, {
       zoomControl: false,
-      // doubleClickZoom: false,
-      // boxZoom: false,
-      // keyboard: false,
-      // scrollWheelZoom: false,
-      // zoomAnimation: false,
-      // touchZoom: false,
-      // zoomSnap: 0.1,
+      doubleClickZoom: false,
     });
     this.map.setView([65.505, 12], 5);
+    nauticalScale({ nautic: true, metric: false, imperial: false, maxWidth: 300 }).addTo(this.map);
   }
 
   setLayer(id: string): void {
@@ -60,6 +67,7 @@ export class MapWidget {
     this.removeAllTrackLines()
 
     let skippNextPoint = false;
+    let lastMinute = 0;
     for (let i = 0; i < shipTrack.length - 1; i++) {
       if (skippNextPoint) {
         i++
@@ -75,37 +83,84 @@ export class MapWidget {
         skippNextPoint = true
       }
 
+      const timestamp = new Date(start[1] + 'z')
+      const lat = start[3]
+      const lng = start[2]
+      const heading = start[4]
+      const speed = start[5]
+
+      // marker
+
+      // Only draw one point every minute
+      if (lastMinute !== timestamp.getMinutes()) {
+        this.addMarker(lat, lng, timestamp, heading, speed);
+        lastMinute = timestamp.getMinutes()
+      }
+
+      // line
       let color = 'orange';
-      // if (start[5] > 10) {
-      //   color = 'orange';
-      // }
-      // if (start[5] > 20) {
-      //   color = 'red';
-      // }
 
       const polyline = L.polyline([
         [start[3], start[2]],
         [end[3], end[2]]
-      ], { color }).addTo(this.map)
+      ], { color, weight: 5 }).addTo(this.map)
 
       this.tracks.push(polyline)
 
-      polyline.on('mouseover', (e) => {
-        L.popup()
-          .setLatLng(e.latlng)
-          .setContent(`
-            <b>Speed: </b>${start[5]} knots<br>
-            <b>Calculated Speed: </b>${start[7]} kph<br>
-            <b>Time: </b>${new Date(start[1] as string).toLocaleString()}<br>
-            <b>Distance: </b>${start[9]}
-          `)
-          .openOn(this.map)
-      })
+      polyline.on('click', (e) => this.onPolylineClick(e, heading, speed, timestamp));
     }
+  }
+
+  addMarker(lat: number, lng: number, timestamp: Date, heading: number, speed: number) {
+    const shipMarker = L.marker([lat, lng], {
+      // function extended in ./leaflet/RotatedMarker.js
+      //@ts-ignore
+      rotationAngle: heading,
+      icon: this.shipIcon
+    }).addTo(this.map)
+    this.markers.push(shipMarker)
+    shipMarker.on('click', (e) => this.onPolylineClick(e, heading, speed, timestamp));
+
+    const formattedTime = timestamp.toLocaleString("no", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+
+    const icon = L.divIcon({
+      className: 'timestamp-marker',
+      html: `<div>${formattedTime}</div>`,
+      iconSize: [60, 30],
+      iconAnchor: [32, -20]
+    });
+
+    const timeMarker = L.marker([lat, lng], { icon }).addTo(this.map);
+    this.markers.push(timeMarker);
+    timeMarker.on('click', (e) => this.onPolylineClick(e, heading, speed, timestamp));
+  }
+
+  onPolylineClick(e: L.LeafletMouseEvent, heading: number, speed: number, timestamp: Date) {
+    L.popup()
+        .setLatLng(e.latlng)
+        .setContent(`
+          <b>COG: </b>${Math.round(heading).toString().padStart(3, '0')}°<br>
+          <b>SOG: </b>${speed} knots<br>
+          <b>Time: </b>${timestamp.toLocaleString("no", {
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit"
+        })}<br>
+        `)
+        .openOn(this.map)
   }
 
   removeAllTrackLines() {
     this.tracks.forEach(track => this.map.removeLayer(track))
     this.tracks = []
+
+    this.markers.forEach(marker => this.map.removeLayer(marker))
+    this.markers = []
+
+    this.icons.forEach(icon => this.map.removeLayer(icon))
+    this.icons = []
   }
 }
